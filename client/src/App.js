24f430a5a94a2b442c1b53386ebbba7733f66706
rajeshsaharan <rajeshsharan1607@gmail.com>
2022-11-13 11:13:36 +0530

    docker-nginx-react-node

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+    <h1>this is our frontend application for testing</h1>
+    <p>server is running on nginx /aws using docker</p>
+    </>
+  );
+}
+
+export default App;
